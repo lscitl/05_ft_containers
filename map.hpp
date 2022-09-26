08be94a7b63a7d579de20c6c7aef67bc0d1d2e88
Seungduk Seo <seseo@student.42seoul.kr>
2022-09-27 01:31:32 +0900

    map prototypes are defined.

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 15:11:07 by seseo             #+#    #+#             */
-/*   Updated: 2022/09/23 23:52:20 by seseo            ###   ########.fr       */
+/*   Updated: 2022/09/26 17:02:45 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #define __MAP_H__
 
 // #include <map>
-
+#include <functional>
 #include "iterator.hpp"
 #include "type_traits.hpp"
 #include "lexico_cmp.hpp"
@@ -23,30 +23,96 @@
 
 namespace ft {
 
-template <class Key, class T, class Compare = less<Key>,
+template <class TreeIterator>
+class map_iterator {
+	typedef typename TreeIterator::NodeTypes NodeTypes;
+
+	TreeIterator i;
+
+   public:
+	typedef typename NodeType::value_type                value_type;
+	typedef typename iterator_traits<T>::pointer         pointer;
+	typedef typename iterator_traits<T>::reference       reference;
+	typedef typename iterator_traits<T>::difference_type difference_type;
+	typedef typename std::bidirectional_iterator_tag     iterator_category;
+
+   protected:
+	pointer _current;
+
+   public:
+	map_iterator() : _current( NULL ) {
+	}
+
+	explicit map_iterator( pointer x ) : _current( x ) {
+	}
+
+	template <class U>
+	map_iterator( const map_iterator<U>& u ) : _current( u.base() ) {
+	}
+
+	template <class U>
+	map_iterator& operator=( const map_iterator<U>& u ) {
+		_current = u.base();
+		return *this;
+	}
+
+	pointer base() const {
+		return _current;
+	};
+
+	reference operator*() const {
+		return *_current;
+	}
+
+	pointer operator->() const {
+		return &( operator*() );
+	}
+
+	map_iterator& operator++() {
+		++_current;
+		return *this;
+	}
+
+	map_iterator operator++( int ) {
+		map_iterator ret( *this );
+		++_current;
+		return ret;
+	}
+
+	map_iterator& operator--() {
+		--_current;
+		return *this;
+	}
+
+	map_iterator operator--( int ) {
+		map_iterator ret( *this );
+		--_current;
+		return ret;
+	}
+};
+
+template <class Key, class T, class Compare = std::less<Key>,
 		  class Allocator = std::allocator<pair<const Key, T> > >
 class map {
    public:
 	// types:
-	typedef Key                                      key_type;
-	typedef T                                        mapped_type;
-	typedef pair<const key_type, mapped_type>        value_type;
-	typedef Compare                                  key_compare;
-	typedef Allocator                                allocator_type;
-	typedef typename allocator_type::reference       reference;
-	typedef typename allocator_type::const_reference const_reference;
+	typedef Key                               key_type;
+	typedef T                                 mapped_type;
+	typedef pair<const key_type, mapped_type> value_type;
+	typedef Compare                           key_compare;
+	typedef Allocator                         allocator_type;
+	typedef value_type&                       reference;
+	typedef const value_type&                 const_reference;
+
 	typedef typename allocator_type::pointer         pointer;
 	typedef typename allocator_type::const_pointer   const_pointer;
 	typedef typename allocator_type::size_type       size_type;
 	typedef typename allocator_type::difference_type difference_type;
 
-	typedef implementation - defined              iterator;
-	typedef implementation - defined              const_iterator;
-	typedef std::reverse_iterator<iterator>       reverse_iterator;
-	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-	typedef unspecified                           node_type;  // C++17
-	typedef INSERT_RETURN_TYPE<iterator, node_type>
-		insert_return_type;  // C++17
+	typedef ft::map_iterator<T>                  iterator;
+	typedef ft::map_iterator<const T>            const_iterator;
+	typedef ft::reverse_iterator<iterator>       reverse_iterator;
+	typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;
 
 	class value_compare : public binary_function<value_type, value_type, bool> {
 		friend class map;
@@ -61,87 +127,50 @@ class map {
 	};
 
 	// construct/copy/destroy:
-	map() noexcept( is_nothrow_default_constructible<allocator_type>::value&&
-						is_nothrow_default_constructible<key_compare>::value&&
-							is_nothrow_copy_constructible<key_compare>::value );
-	explicit map( const key_compare& comp );
-	map( const key_compare& comp, const allocator_type& a );
+	explicit map( const key_compare&    comp = key_compare(),
+				  const allocator_type& alloc = allocator_type() );
 	template <class InputIterator>
 	map( InputIterator first, InputIterator last,
-		 const key_compare& comp = key_compare() );
-	template <class InputIterator>
-	map( InputIterator first, InputIterator last, const key_compare& comp,
-		 const allocator_type& a );
-	map( const map& m );
-	map( map&& m ) noexcept(
-		is_nothrow_move_constructible<allocator_type>::value&&
-			is_nothrow_move_constructible<key_compare>::value );
-	explicit map( const allocator_type& a );
-	map( const map& m, const allocator_type& a );
-	map( map&& m, const allocator_type& a );
-	map( initializer_list<value_type> il,
-		 const key_compare&           comp = key_compare() );
-	map( initializer_list<value_type> il, const key_compare& comp,
-		 const allocator_type& a );
+		 const key_compare&    comp = key_compare(),
+		 const allocator_type& alloc = allocator_type() );
+	map( const map& x );
 	~map();
 
 	map& operator=( const map& m );
-	map& operator=( map&& m ) noexcept(
-		allocator_type::propagate_on_container_move_assignment::value&&
-				is_nothrow_move_assignable<allocator_type>::value&&
-				is_nothrow_move_assignable<key_compare>::value );
-	map& operator=( initializer_list<value_type> il );
 
 	// iterators:
-	iterator       begin() noexcept;
-	const_iterator begin() const noexcept;
-	iterator       end() noexcept;
-	const_iterator end() const noexcept;
+	iterator       begin();
+	const_iterator begin() const;
+	iterator       end();
+	const_iterator end() const;
 
-	reverse_iterator       rbegin() noexcept;
-	const_reverse_iterator rbegin() const noexcept;
-	reverse_iterator       rend() noexcept;
-	const_reverse_iterator rend() const noexcept;
-
-	const_iterator         cbegin() const noexcept;
-	const_iterator         cend() const noexcept;
-	const_reverse_iterator crbegin() const noexcept;
-	const_reverse_iterator crend() const noexcept;
+	reverse_iterator       rbegin();
+	const_reverse_iterator rbegin() const;
+	reverse_iterator       rend();
+	const_reverse_iterator rend() const;
 
 	// capacity:
-	bool      empty() const noexcept;
-	size_type size() const noexcept;
-	size_type max_size() const noexcept;
+	bool      empty() const;
+	size_type size() const;
+	size_type max_size() const;
 
 	// element access:
 	mapped_type& operator[]( const key_type& k );
-	mapped_type& operator[]( key_type&& k );
-
-	mapped_type&       at( const key_type& k );
-	const mapped_type& at( const key_type& k ) const;
 
 	// modifiers:
-	template <class... Args>
-	pair<iterator, bool> emplace( Args&&... args );
-	template <class... Args>
-	iterator emplace_hint( const_iterator position, Args&&... args );
-	pair<iterator, bool> insert( const value_type& v );
-	template <class P>
-	pair<iterator, bool> insert( P&& p );
-	iterator             insert( const_iterator position, const value_type& v );
-	template <class P>
-	iterator insert( const_iterator position, P&& p );
+	pair<iterator, bool> insert( const value_type& val );
+	iterator             insert( iterator position, const value_type& val );
 	template <class InputIterator>
 	void insert( InputIterator first, InputIterator last );
-	void insert( initializer_list<value_type> il );
 
-	iterator  erase( const_iterator position );
+	void      erase( iterator position );
 	size_type erase( const key_type& k );
-	iterator  erase( const_iterator first, const_iterator last );
-	void      clear() noexcept;
+	void      erase( iterator first, iterator last );
+
+	void clear();
 
 	// observers:
-	allocator_type get_allocator() const noexcept;
+	allocator_type get_allocator() const;
 	key_compare    key_comp() const;
 	value_compare  value_comp() const;
 
@@ -185,9 +214,8 @@ bool operator<=( const map<Key, T, Compare, Allocator>& x,
 
 // specialized algorithms:
 template <class Key, class T, class Compare, class Allocator>
-void swap(
-	map<Key, T, Compare, Allocator>& x,
-	map<Key, T, Compare, Allocator>& y ) noexcept( noexcept( x.swap( y ) ) );
+void swap( map<Key, T, Compare, Allocator>& x,
+		   map<Key, T, Compare, Allocator>& y );
 
 }  // namespace ft
 
