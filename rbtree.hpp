306a7d812c670fd71389f3a38e5df7bdb817e0fe
Seungduk Seo <seseo@student.42seoul.kr>
2022-10-03 00:12:35 +0900

    rbtree insert WIP

--- rbtree.hpp
+++ rbtree.hpp
@@ -6,7 +6,7 @@
 /*   By: seseo <seseo@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 16:51:25 by seseo             #+#    #+#             */
-/*   Updated: 2022/09/30 22:26:41 by seseo            ###   ########.fr       */
+/*   Updated: 2022/10/03 00:11:16 by seseo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@
 // 4. A red node does not have a red child.
 // 5. Every path from a given node to any of its descendant NIL nodes goes
 // through the same number of black nodes.
+
+namespace ft {
 
 typedef enum { RED, BLACK } color_t;
 
@@ -118,30 +120,31 @@ struct rbtree_iterator_base {
 	}
 };
 
-template <class T, class Compare, class Allocator>
-class rbtree
-// : public rbtree_header<T>,
-// 			   public rbtree_key_compare<Compare>,
-// 			   public rbtree_node_allocator<Allocator>
-{
+template <class T,
+		  class Compare = std::less<typename get_node_type<T>::key_type>,
+		  class Allocator = std::allocator<T> >
+class rbtree {
    public:
-	typedef typename get_node_type<T>::key_type                key_type;
-	typedef typename get_node_type<T>::value_type              value_type;
-	typedef Compare                                            value_compare;
-	typedef typename Allocator::rebind<rbtree_node<T> >::other allocator_type;
-	typedef typename allocator_type::pointer                   pointer;
-	typedef typename allocator_type::const_pointer             const_pointer;
-	typedef typename allocator_type::size_type                 size_type;
-	typedef typename allocator_type::difference_type           difference_type;
-	typedef rbtree_iterator_base                               iterator;
-	typedef const iterator                                     const_iterator;
+	typedef typename get_node_type<T>::key_type   key_type;
+	typedef typename get_node_type<T>::value_type value_type;
+	typedef Compare                               value_compare;
+	typedef typename Allocator::template rebind<rbtree_node<T> >::other
+													 allocator_type;
+	typedef typename allocator_type::pointer         pointer;
+	typedef typename allocator_type::const_pointer   const_pointer;
+	typedef typename allocator_type::size_type       size_type;
+	typedef typename allocator_type::difference_type difference_type;
+	typedef rbtree_iterator_base                     iterator;
+	typedef const iterator                           const_iterator;
 
    protected:
-	typedef rbtree_node_base*                    link_type;
-	typedef const rbtree_node_base*              const_link_type;
 	typedef typename get_node_type<T>::node_type node_type;
+	typedef node_type*                           link_type;
+	typedef const node_type*                     const_link_type;
 
    private:
+	node_type      __end_node;
+	link_type      _root_node;
 	link_type      _begin_node;
 	link_type      _end_node;
 	value_compare  _comp;
@@ -156,10 +159,59 @@ class rbtree
 		return ( sizeof( key_type ) );
 	}
 
+	rbtree()
+		: __end_node(),
+		  _begin_node( &__end_node ),
+		  _end_node( &__end_node ),
+		  _comp( Compare() ),
+		  _size( 0 ),
+		  _alloc( Allocator() ) {
+		__end_node.color = BLACK;
+		__end_node.left = NULL;
+		__end_node.right = NULL;
+		__end_node.parent = NULL;
+	}
+	~rbtree() {
+	}
+
+	ft::pair<iterator, bool> insert( const value_type& val ) {
+		link_type* new_node = _alloc.allocate( 1 );
+		_alloc.construct( new_node, val );
+		new_node->left = NULL;
+		new_node->right = NULL;
+		new_node->parent = NULL;
+
+		if ( _root_node == NULL ) {
+			_root_node = new_node;
+			new_node->right = _end_node;
+			_end_node->parent = _root_node;
+			++_size;
+			return make_pair( iterator( _root_node ), true );
+		}
+		link_type* cur_node = _root_node;
+		link_type* prev_node = _root_node;
+		if ( _comp( cur_node->value_field, val ) ) {
+			cur_node = cur_node->right;
+		}
+
+		while ( cur_node && _comp( cur_node->value_field, val ) ) {
+			prev_node = cur_node;
+			cur_node = cur_node->right;
+		}
+		_begin_node->left = _end_node;
+		_begin_node->right = _end_node;
+		return make_pair( iterator( new_node ), true );
+	}
+
+	link_type get_begin_node() {
+		return ( _begin_node );
+	}
 	//    public:
 	// 	rbtree();
 	// 	~rbtree();
 	// 	rbtree( const rbtree& ref );
 };
+
+}  // namespace ft
 
 #endif
